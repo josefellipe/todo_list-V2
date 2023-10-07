5cd02bf626c3af497df3565363847d850d9bb243
josefellipe <josefserqueira@gmail.com>
2023-10-07 15:36:35 -0300

    create a update task

--- task/views.py
+++ task/views.py
@@ -3,6 +3,7 @@ from .forms import TaskForm
 from .models import Task
 
 def home(request):
+    tasks               = Task.objects.all()
     tasks_todo          = Task.objects.filter(status='todo')
     tasks_in_progress   = Task.objects.filter(status='in_progress')
     tasks_is_complete   = Task.objects.filter(status='is_complete')
@@ -11,6 +12,8 @@ def home(request):
 
     context = {
         'form': form,
+        'form_teste': {'form': form},
+        'all_tasks': tasks,
         'tasks_todo': tasks_todo,
         'tasks_in_progress': tasks_in_progress,
         'tasks_is_complete': tasks_is_complete
